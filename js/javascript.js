846e129a2aadef7ea6afddafb79a0c43d0c89e18
abferrari <andrea.bonette.ferrari@gmail.com>
2016-04-04 21:11:39 -0400

    Lobster Animation
    Testing animation with SVG, CSS and JQuery. It's working!

--- js/javascript.js
+++ js/javascript.js
@@ -5,7 +5,15 @@ $(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
       if (target.length) {
+        if($(this).attr('class') == "button button-claws") {
+          $('#lobster-claws').animate({
+            marginTop:$('#lobster-claws').height()+100
+          }, 1000);
+          $('html,body').delay(700);
+        }
+
         $('html,body').animate({
           //the top - # sets it that # of pixels above div (for nav)
           scrollTop: target.offset().top-0
